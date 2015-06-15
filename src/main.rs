d8f7e1b38da71c0f54170dc6074cd10843994be8
Corey Richardson <corey@octayn.net>
2015-06-15 05:01:51 -0400

    Snapshot

--- src/main.rs
+++ src/main.rs
@@ -1,160 +1,40 @@
-use std::collections::HashMap;
-use std::iter::IntoIterator;
+#![feature(io)]
+extern crate unif;
+extern crate env_logger;
 
-#[derive(Eq, PartialEq, Hash, Clone, Debug)]
-enum SigmaTerm {
-    Var(u32),
-    Func(u32, Vec<SigmaTerm>),
-}
+use std::io::prelude::*;
 
-#[derive(Eq, PartialEq, Hash, Clone, Debug)]
-enum CapTerm {
-    Union(Vec<CapTerm>),
-    Intersection(Vec<CapTerm>),
-    Func(u32, Vec<CapTerm>),
-    Cap(Box<CapTerm>),
-}
-
-impl CapTerm {
-    fn push(&mut self, t: CapTerm) {
-        match *self {
-            CapTerm::Union(ref mut ts) => ts.push(t),
-            CapTerm::Intersection(ref mut ts) => ts.push(t),
-            CapTerm::Cap(..) => panic!("Tried to push into a CapTerm::Cap"),
-            CapTerm::Func(..) => panic!("Tried to push into a CapTerm::Func"),
+fn single_match<I: Iterator<Item=char>>(p: &mut unif::parse::Parser<I>, c: &unif::Context) -> Result<(), unif::parse::ParseError> {
+    let from = try!(p.parse_sigma_term());
+    try!(p.eat(unif::parse::Token::Word("=".into(), true)));
+    let to = try!(p.parse_cap_term());
+    let substs = c.match_(&from, &to);
+    if substs.len() > 0 {
+        for subst in substs {
+            println!("Solution:");
+            for (k, v) in subst {
+                p.print_sigma_term(&unif::SigmaTerm::Var(k));
+                print!(" |-> ");
+                p.print_cap_term(&v);
+                println!("");
+            }
         }
+    } else {
+        println!("No solution.");
     }
-}
-
-type Subst = HashMap<u32, CapTerm>;
-
-struct Context {
-    // Functions we can introduce, and their arity.
-    constructors: HashMap<u32, usize>,
-    // max_func: u32,
-    // List of rules eqv. to the form "Hypothesis |- Conclusion"
-    // destructors: Vec<(Vec<SigmaTerm>, Vec<SigmaTerm>)>,
-}
-
-// FIXME: backtracking like this is ultra inefficient. Consider using a HAMT-backed persistent map,
-// or having capmatch push into a (possibly empty) list of "insert these things for me please",
-// but we probably want a HAMT as backtracking can go quite deep.
-
-impl Context {
-    fn capmatch(&self, s: &SigmaTerm, t: &CapTerm, into: &mut Subst) -> bool {
-        match (s, t) {
-            (&SigmaTerm::Func(sf, _), &CapTerm::Func(cf, _)) if sf != cf => {
-                // clash
-                false
-            },
-            (&SigmaTerm::Func(_, ref sargs), &CapTerm::Func(_, ref cargs)) => {
-                // decomp
-                for (sarg, carg) in sargs.iter().zip(cargs) {
-                    let save = into.clone();
-                    if !self.capmatch(sarg, carg, into) {
-                        *into = save; // backtrack
-                        return false;
-                    }
-                }
-                true
-            },
-            (&SigmaTerm::Func(sf, ref sargs), &CapTerm::Cap(ref capset)) if self.constructors.contains_key(&sf) => {
-                // constr decomp
-                // I'm not sure if the "sigma" in "C sigma" is important. What it means is that we
-                // have an instantiation of one of the rules, which... looks a lot like a normal
-                // function application! One thing to note is that the sigma may not be complete,
-                // and thus `sargs` could have unbound variables that came from the rule, but I
-                // also think that's not an important detail.
-                let mut all = true;
-                let save = into.clone();
-                for arg in sargs {
-                   if !self.capmatch(arg, t, into) {
-                       *into = save; // backtrack
-                       all = false;
-                       break
-                   }
-                }
-                let save = into.clone();
-                if !self.capmatch(s, capset, into) {
-                    *into = save; // backtrack
-                    false
-                } else {
-                    all
-                }
-            },
-            (_, &CapTerm::Union(ref stuffs)) => {
-                // split
-                let mut any = false;
-                for un in stuffs {
-                    let save = into.clone();
-                    if !self.capmatch(s, un, into) {
-                        *into = save;
-                    } else {
-                        any = true;
-                    }
-                }
-                any
-            },
-            (&SigmaTerm::Var(x), _) => {
-                // No rule for this, but we've solved for x.
-                into.entry(x).or_insert(CapTerm::Intersection(Vec::with_capacity(2))).push(t.clone());
-                true
-            }
-            (&SigmaTerm::Func(_, ref fargs), &CapTerm::Cap(ref capset)) => {
-                // Cap decomp
-                // This isn't a rule anymore, to handle destructors, but the actual destructor rule
-                // is SO AWFUL that I can't bear to implement it yet.
-                let save = into.clone();
-                let mut a = true;
-                for arg in fargs {
-                    if !self.capmatch(arg, t, into) {
-                        a = false;
-                        *into = save;
-                        break;
-                    }
-                }
-                let save = into.clone();
-                let mut b = true;
-                if !self.capmatch(s, capset, into) {
-                    *into = save;
-                    b = true;
-                }
-                a || b
-            }
-            /*
-            (_, &CapTerm::Cap(ref capset)) => {
-                // destr decomp
-                // See above comments about sigma. Additionally, I'm not *quite* sure why each
-                // sigma isn't unique... In this case, we care a lot about what sigma actually is
-                // because that determines how we substitute back into the hypothesis!
-                //
-                // We implement this right now by checking, for each rule, whether it's possible to
-                // match some selection of terms from the Cap set against the conclusion.
-                //
-                // This sucks a lot.
-                let mut relevant_rules = self.destructors.iter().filter(|(h, c)| c.len() <= capset.len());
-                for term in capset {
-                    if let &CapTerm::Func(fc, args) = term {
-
-                    }
-                }
-            }
-            */
-
-            crap => panic!("The ruleset is not complete! capmatch saw {:?} but didn't handle it", crap),
-        }
-    }
+    try!(p.eat(unif::parse::Token::Dot));
+    Ok(())
 }
 
 fn main() {
-    let c = Context {
-        constructors: vec![(0, 0), (1, 0), (2, 1), (3, 1), (4, 2), (5, 3)].into_iter().collect()
-    };
-    let mut final_subst = HashMap::new();
-    // f(a, x) c? Cap(f(a, f(a, a)) u b)
-    c.capmatch(&SigmaTerm::Func(2, vec![SigmaTerm::Func(0, vec![]), SigmaTerm::Var(0)]),
-               &CapTerm::Cap(Box::new(CapTerm::Union(vec![CapTerm::Func(2, vec![CapTerm::Func(0, vec![]), CapTerm::Func(2, vec![CapTerm::Func(0, vec![]), CapTerm::Func(0, vec![])])]), CapTerm::Func(1, vec![])]))),
-               &mut final_subst);
-    println!("Substitution is: {:?}", final_subst);
-    // x -> f(a, a) ??? Should be x -> Cap(b u f(a, a)) I think...
+    env_logger::init().unwrap();
+    println!("Enter terms to unify! Like so: f(x) = Cap(a | f(b)).");
+    let mut p = unif::parse::Parser::new(std::io::stdin().chars().map(|a| a.unwrap()));
+    let c = unif::Context::new();
+    loop {
+        match single_match(&mut p, &c) {
+            Ok(..) => { },
+            Err(e) => println!("Error: {:?}", e),
+        }
+    }
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,47 +1,304 @@
 use std::collections::HashMap;
+use std::iter::Peekable;
 
-pub struct Parser {
-    vars: HashMap<String, u32>,
-    syms: HashMap<String, u32>,
-    funcs: HashMap<String, u32>,
+use self::Token::*;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Token {
+    Word(String, bool),
+    LParen,
+    RParen,
+    Dot,
+    Comma,
+    Cap,
 }
 
-enum State {
-    Initial,
-    EatingVar,
-    EatingSym,
-    EatingFunc,
+impl Token {
+    fn is_word(&self, s: &str) -> bool {
+        match *self {
+            Word(ref t, _) => s == t,
+            _ => false
+        }
+    }
 }
 
-impl Parser {
-    pub fn new() -> Parser {
-        Parser { vars: HashMap::new(), syms: HashMap::new(), funcs: HashMap::new() }
+pub struct Lexer<I: Iterator<Item=char>> {
+    it: Peekable<I>,
+}
+
+fn is_toksep(c: char) -> bool {
+    c == '(' || c == ')' || c == ',' || c == '.' || c.is_whitespace()
+}
+
+impl<I: Iterator<Item=char>> Lexer<I> {
+    pub fn new(i: I) -> Lexer<I> {
+        Lexer {
+            it: i.peekable()
+        }
     }
 
-    pub fn parse(&mut self, x: &str) -> ::Term {
-        //let mut term;
-        let mut state = State::Initial;
+    fn eat_word(&mut self) -> Token {
+        let mut acc = String::new();
+        while !is_toksep(*self.it.peek().unwrap_or(&'.')) {
+            acc.push(self.it.next().unwrap());
+        }
+        let c = !acc.chars().next().unwrap().is_lowercase();
+        if "Cap" == acc {
+            Cap
+        } else {
+            Word(acc, c)
+        }
+    }
+}
 
-        let mut it = x.char_indices();
+impl<I: Iterator<Item=char>> Iterator for Lexer<I> {
+    type Item = Token;
 
-        while let Some((idx, c)) = it.next() {
-            match c {
-                c if c.is_uppercase() => {
-                    let var_id = self.vars.len() as u32;
-                    let mut last = it.by_ref().skip_while(|&(i, c)| c.is_alphanumeric());
-                    let name = String::from(&x[idx..]last.next().map(|(x, c)| x).unwrap_or(x.len()));
-                    self.vars.insert(name, var_id);
-                },
-                c if c.is_lowercase() => {
-                    let var_id = self.syms.len() as u32;
-                    let mut last = it.by_ref().skip_while(|&(i, c)| c.is_alphanumeric());
-                    let name = String::from(&x[idx..last.next().map(|(x, c)| x).unwrap_or(x.len()));
-                    self.syms.insert(String::from(&x[idx..last.next().map(|(x, c)| x).unwrap_or(x.len())]), var_id);
-                },
-                _ => { }
+    fn next(&mut self) -> Option<Token> {
+        while let Some(&c) = self.it.peek() {
+            if c.is_whitespace() { self.it.next(); continue }
+            if !is_toksep(c) {
+                return Some(self.eat_word());
+            } else {
+                match self.it.next().unwrap() {
+                    '(' => return Some(LParen),
+                    ')' => return Some(RParen),
+                    '.' => return Some(Dot),
+                    ',' => return Some(Comma),
+                    _ => unreachable!()
+                }
             }
         }
+        None
+    }
+}
 
-        ::Term::Var(0)
+
+pub struct Parser<I: Iterator<Item=char>> {
+    pub vars: Vec<String>,
+    pub funcs: Vec<String>,
+    vars_inverse: HashMap<String, u32>,
+    funcs_inverse: HashMap<String, u32>,
+    arities: Vec<u32>,
+    l: Peekable<Lexer<I>>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ParseError {
+    ArityMismatch(String),
+    Done,
+    BadSyntax(Token, String),
+    WantedMore,
+}
+
+// A -> B A'
+// A' -> ∪ A | epsilon
+// B -> C B'
+// B' -> ∩ B | epsilon
+// C -> Word | Word ( Args ) | Cap ( A )
+// Args -> A Args'
+// Args' -> ',' Args | epsilon
+
+impl<I: Iterator<Item=char>> Parser<I> {
+    fn intern(&mut self, n: String, is_const: bool) -> u32 {
+        if is_const {
+            let mut new = false;
+            let l = match self.funcs_inverse.get(&n) {
+                Some(n) => *n,
+                None => { new = true; self.funcs.push(n.clone()); self.arities.push(0); self.funcs.len() as u32 - 1 }
+            };
+            if new {
+                self.funcs_inverse.insert(n, l);
+            }
+            l
+        } else {
+            let mut new = false;
+            let l = match self.vars_inverse.get(&n) {
+                Some(n) => *n,
+                None => { new = true; self.vars.push(n.clone()); self.vars.len() as u32 - 1 }
+            };
+            if new {
+                self.funcs_inverse.insert(n, l);
+            }
+            l
+        }
+    }
+
+    pub fn eat(&mut self, t: Token) -> Result<(), ParseError> {
+        if self.l.peek() == Some(&t) {
+            self.l.next();
+            Ok(())
+        } else {
+            Err(ParseError::BadSyntax(self.l.next().unwrap_or(Dot), format!("Expected {:?}", t)))
+        }
+    }
+
+    pub fn new(i: I) -> Parser<I> {
+        let mut p = Parser {
+            vars: Vec::new(),
+            funcs: Vec::new(),
+            vars_inverse: HashMap::new(),
+            funcs_inverse: HashMap::new(),
+            arities: Vec::new(),
+            l: Lexer::new(i).peekable(),
+        };
+        p.intern("p".into(), true);
+        p.arities.push(2);
+        p.intern(".px.".into(), false);
+        p.intern(".py.".into(), false);
+        p.intern("e".into(), true);
+        p.arities.push(2);
+        p.intern(".em.".into(), false);
+        p.intern(".ek.".into(), false);
+        p
+    }
+
+    pub fn parse_cap_term(&mut self) -> Result<::CapTerm, ParseError> {
+        let mut un = vec![try!(self.parse_cap_b())];
+        while self.l.peek().map_or(false, |t| t.is_word("|")) {
+            self.l.next();
+            un.push(try!(self.parse_cap_b()));
+        }
+        if un.len() == 1 {
+            Ok(un.swap_remove(0))
+        } else {
+            Ok(::CapTerm::Union(un))
+        }
+    }
+
+    fn parse_cap_b(&mut self) -> Result<::CapTerm, ParseError> {
+        let mut un = vec![try!(self.parse_cap_c())];
+        while self.l.peek().map_or(false, |t| t.is_word("&")) {
+            self.l.next();
+            un.push(try!(self.parse_cap_c()));
+        }
+        if un.len() == 1 {
+            Ok(un.swap_remove(0))
+        } else {
+            Ok(::CapTerm::Union(un))
+        }
+    }
+
+    fn parse_cap_c(&mut self) -> Result<::CapTerm, ParseError> {
+        match self.l.next() {
+            Some(Word(name, _)) => {
+                if self.l.peek() == Some(&LParen) {
+                    try!(self.eat(LParen));
+                    let fs = self.intern(name, true);
+                    let mut args = Vec::new();
+                    while self.l.peek() != Some(&RParen) {
+                        args.push(try!(self.parse_cap_term()));
+                        if self.l.peek() != Some(&RParen) {
+                            try!(self.eat(Comma));
+                        }
+                    }
+                    if self.arities[fs as usize] == 0 {
+                        self.arities[fs as usize] = args.len() as u32;
+                    } else if self.arities[fs as usize] != args.len() as u32 {
+                        return Err(ParseError::ArityMismatch(format!("in call to {}, expected {} arguments by earlier usage, got {}",
+                                                                     self.funcs[fs as usize], self.arities[fs as usize], args.len())));
+                    }
+                    try!(self.eat(RParen));
+                    Ok(::CapTerm::Func(fs, args))
+                } else {
+                    Ok(::CapTerm::Func(self.intern(name, true), vec![]))
+                }
+            },
+            Some(Dot) => return Err(ParseError::Done),
+            Some(Cap) => {
+                try!(self.eat(LParen));
+                let t = ::CapTerm::Cap(Box::new(try!(self.parse_cap_term())));
+                try!(self.eat(RParen));
+                Ok(t)
+            },
+            Some(t) => Err(ParseError::BadSyntax(t, "expected a function, variable, or .".into())),
+            None => Err(ParseError::WantedMore),
+        }
+    }
+
+    pub fn parse_sigma_term(&mut self) -> Result<::SigmaTerm, ParseError> {
+        match self.l.next() {
+            Some(Word(name, is_const)) => {
+                if self.l.peek() == Some(&LParen) {
+                    try!(self.eat(LParen));
+                    let fs = self.intern(name, true);
+                    let mut args = Vec::new();
+                    while self.l.peek() != Some(&RParen) {
+                        args.push(try!(self.parse_sigma_term()));
+                        if self.l.peek() != Some(&RParen) {
+                            try!(self.eat(Comma));
+                        }
+                    }
+                    if self.arities[fs as usize] == 0 {
+                        self.arities[fs as usize] = args.len() as u32;
+                    } else if self.arities[fs as usize] != args.len() as u32 {
+                        return Err(ParseError::ArityMismatch(format!("expected {} arguments to {} by earlier usage, got {}", self.funcs[fs as usize], self.arities[fs as usize], args.len())));
+                    }
+                    try!(self.eat(RParen));
+                    Ok(::SigmaTerm::Func(fs, args))
+                } else {
+                    Ok(::SigmaTerm::Var(self.intern(name, is_const)))
+                }
+            },
+            Some(Dot) => return Err(ParseError::Done),
+            Some(t) => Err(ParseError::BadSyntax(t, "expected a function, variable, or .".into())),
+            None => Err(ParseError::WantedMore),
+        }
+    }
+
+    pub fn print_cap_term(&self, t: &::CapTerm) {
+        match *t {
+            ::CapTerm::Union(ref args) => {
+                for arg in args.iter().take(args.len()-1) {
+                    self.print_cap_term(arg);
+                    print!(" | ");
+                }
+                self.print_cap_term(args.last().unwrap());
+            }
+            ::CapTerm::Intersection(ref args) => {
+                for arg in args.iter().take(args.len()-1) {
+                    self.print_cap_term(arg);
+                    print!(" & ");
+                }
+                self.print_cap_term(args.last().unwrap());
+            }
+            ::CapTerm::Func(x, ref args) => {
+                print!("{}", &self.funcs[x as usize]);
+                if args.len() > 0 {
+                    print!("(");
+                    for arg in args.iter().take(args.len()-1) {
+                        self.print_cap_term(arg);
+                        print!(", ");
+                    }
+                    self.print_cap_term(args.last().unwrap());
+                    print!(")");
+                }
+            }
+            ::CapTerm::Cap(ref arg) => {
+                print!("Cap(");
+                self.print_cap_term(arg);
+                print!(")");
+            }
+        }
+    }
+
+    pub fn print_sigma_term(&self, t: &::SigmaTerm) {
+        match *t {
+            ::SigmaTerm::Func(x, ref args) => {
+                print!("{}", &self.funcs[x as usize]);
+                if args.len() > 0 {
+                    print!("(");
+                    for arg in args.iter().take(args.len()-1) {
+                        self.print_sigma_term(arg);
+                        print!(", ");
+                    }
+                    self.print_sigma_term(args.last().unwrap());
+                    print!(")");
+                }
+            }
+            ::SigmaTerm::Var(x) => {
+                print!("{}", &self.vars[x as usize]);
+            }
+        }
     }
 }
